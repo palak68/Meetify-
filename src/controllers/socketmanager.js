d194e81da89f51f6db9c14d67dc9ec4c8767ea10
Palak Sharma <siyamukeshsharma@gmail.com>
2026-02-05 14:45:27 +0530

    third day update

--- src/controllers/socketmanager.js
+++ src/controllers/socketmanager.js
@@ -1,5 +1,32 @@
 import { Server } from "socket.io";
+
+let connections = {}
+let messages = {}
+let timeOnline = {}
  export const connectTosocket = (server)=>{
     const io = new Server(server);
+    io.on("connection",(socket)=>{
+        socket.on("join-call",(path)=>{
+
+            if(connection[path]===undefined){
+                connections[path] = []
+            }
+            connections[path].push(socket.id);
+            timeOnline[socket.id] = new Date();
+            for(let a =0; a< connections[path].length; a++){
+                io.to(connections[path][a]).emit("chat-message ", message[path][a] ['data'],
+                     message[path][a]['sender'], message[path][a]['socket-id-sender']);
+           
+            }
+
+        })
+        socket.on("signal", (toId,message)=>{
+            io.to(toId).emit("signal", socket.id, message);
+        })
+        socket.on("chat-message",(data,sender)=>{
+
+        })
+        socket.on ("disconnect",()=>{
+    })
     return io
-}
+} )}
