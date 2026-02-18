2d912c0cbb45d74be5427e1e5e0414ffa7954fc0
Palak Sharma <siyamukeshsharma@gmail.com>
2026-02-18 16:38:08 +0530

    vedio and audio control

--- Frontend/meetify/src/pages/VedioMeet.jsx
+++ Frontend/meetify/src/pages/VedioMeet.jsx
@@ -33,9 +33,94 @@ export default function VedioMeetComponent() {
 //   if(isChrome() === false){
 
 //   }
+let getPermissions = async ()=>{
+  try{
+    const vedioPermission = await navigator.mediaDevices.getUserMedia({video:true});
+    if(vedioPermission){
+      setVedioAvailable(true);
+    } else{
+      setVedioAvailable(false);
+    }
+
+    const audioPermission = await navigator.mediaDevices.getUserMedia({audio:true});
+    if(audioPermission){
+      setAudioAvailable(true);
+    } else{
+      setAudioAvailable(false);
+    }
+    if(navigator.mediaDevices.getDisplayMedia){
+      setScreenAvailable(true);
+    } else{
+      setScreenAvailable(false);
+    }
+
+    if(vedioAvailable || audioAvailable){
+      const userMediaStream = await navigator.mediaDevices.getUserMedia({video:vedioAvailable, audio:audioAvailable});
+      if(userMediaStream){
+        window.localStream = userMediaStream;
+        if(localVedioRef.current){
+          localVedioRef.current.srcObject = userMediaStream;
+        }
+      }
+    }
+  }catch(err){
+console.log( err);
+
+  }
+}
+useEffect(() => 
+  
+  {
+
+    getPermissions();
+  },[])
+
+ let userMediaSuccess = (stream)=>{
+
+ }
+let getuserMedia = ()=>{
+  if(vedio && vedioAvailable || audio && audioAvailable){
+
+    navigator.mediaDevices.getUserMedia({video:vedio, audio:audio})
+   .then(getUserMediaSuccess)
+    .then((stream)=>{}).
+    catch((err)=>{
+      console.log(err);
+    } 
+  )
+  } else{
+    try{
+  let tracks = localVedioRef.current.srcObject.getTracks();
+  tracks.forEach(track => track.stop());
+    }catch(err){
+
+    }
+  }
+
+}
+
+
+  useEffect(() => {
+    if(vedio!== undefined && audio !== undefined ){
+        getMedia();
+    }} ,[vedio, audio])
+  let getMedia =()=>{
+    setVedio(vedioAvailable);
+    setAudio(audioAvailable);
+    connectToSocketServer();
+
+  }
     return (
     <div>
-      {askForUsername==true ? <div></div> : <div></div>}
+      {askForUsername==true ?
+       <div>
+      <h2> Enter your lobby</h2>
+      <TextField id="outlined-basic" label="username " value={username} onChange={(e) => {setUsername(e.target.value)}} variant="outlined" />
+      <Button variant="contained" onClick={connect}>Connect</Button>
+       <div>
+        <vedio ref={localVedioRef} autoPlay muted></vedio>
+       </div>
+      </div> : <div></div>}
     </div>
   );
 }
